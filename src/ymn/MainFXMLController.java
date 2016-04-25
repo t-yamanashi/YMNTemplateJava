77e1fd53f4545ac894df46273f6cfe2d57fdc1aa
山梨智之 <ae86bg5@yahoo.co.jp>
2016-04-25 12:26:16 +0900

    ドラッグ＆ドロップテスト

--- src/ymn/MainFXMLController.java
+++ src/ymn/MainFXMLController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.ClipboardContent;
@@ -38,7 +39,7 @@ public class MainFXMLController implements Initializable {
      * ファイルフォーマットコンボボックス
      */
     @FXML
-    private AnchorPane fileFormatComboBox;
+    private ComboBox<?> fileFormatComboBox;
     
     /**
      * テンプレートファイル名テキストフィールド
